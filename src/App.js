47be1966a1482b8cfcba05cb8f00b150a726e90b
Vesna Leopold <vesna.leopold@gmail.com>
2026-01-13 23:23:31 +0100

    heading added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import "./styles/general/main.scss";
-
+import Heading from "./Components/Heading/Heading";
 export function App() {
   return (
     <>
-      <h1>Parcel React App</h1>
+      <Heading title="Daily todo`s"/>
       <p>Edit <code>src/App.tsx</code> to get started!</p>
     </>
   );
