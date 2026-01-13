db165da5db342eeace5e018104b7646adb785992
Vesna Leopold <vesna.leopold@gmail.com>
2026-01-13 22:19:23 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
-import { App } from './App';
+import { App } from './App.js';
 
-let container = document.getElementById("app")!;
+let container = document.getElementById("app");
 let root = createRoot(container)
 root.render(
   <StrictMode>
