a3be119543dd377c451f886705e6298d5a24b765
Vesna Leopold <vesna.leopold@gmail.com>
2026-01-13 23:49:08 +0100

    container added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
 import "./styles/general/main.scss";
 import Heading from "./Components/Heading/Heading";
 export function App() {
   return (
-    <>
+    <div className={"container"}>
       <Heading title="Daily todo`s"/>
       <p>Edit <code>src/App.tsx</code> to get started!</p>
-    </>
+    </div>
   );
 }
