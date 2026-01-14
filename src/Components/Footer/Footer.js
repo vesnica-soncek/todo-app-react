3cb18d2a68086b18326cc6073f60cb778b470ba2
Vesna Leopold <vesna.leopold@gmail.com>
2026-01-14 01:08:45 +0100

    form and footer added

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./_footer.module.scss";
+
+const Footer = ({title}) => {
+    return (
+        <footer className={styles.footer}>
+            <p>{title} &copy; {new Date().getFullYear().toString()}</p>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/Components/Form/Form.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./_form.module.scss";
+
+const Form = (props) => {
+    return (
+        <form className={styles.form}>
+            {props.children}
+        </form>
+    );
+};
+
+export default Form;
